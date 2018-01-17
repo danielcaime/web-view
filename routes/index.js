b9a99f99f31609fc0b8933413f08248db0c2ae80
caime.daniel <caime.daniel@outlook.com>
2018-01-17 15:58:49 -0300

    prueba1

--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,8 @@ const router = express.Router();
 
 // GET home page for the application
 router.get('/', (_, res) => {
-  res.render('./index', {demo: process.env.DEMO, title: 'Seleccione un producto'});
+  res.write( "hola sarasa",null);
+  //res.render('./index', {demo: process.env.DEMO, title: 'Seleccione un producto'});
 });
 
 
